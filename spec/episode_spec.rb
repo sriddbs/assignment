884a32fad2379ee6c529726128de69df1c4367f0
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-03-24 19:58:25 +0530

    Add spec for episode model

---
+++ spec/episode_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Episode, type: :model do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:plot) }
+  it { should validate_presence_of(:number) }
+  it { should belong_to(:season) }
+end
