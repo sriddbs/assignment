8e9e4e5af43f23cbb655de02f5345593e0eb2b4f
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-03-24 19:53:28 +0530

    Add spec for season model

---
+++ spec/seasons_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Season, type: :model do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:plot) }
+  it { should have_many(:episodes) }
+end
