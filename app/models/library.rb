513698df8c8ce42ad0b25e998739abbb6ef0cbe1
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-04-13 18:39:17 +0530

    Add association between library and library_contents

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,10 +1,10 @@
 class Library < ApplicationRecord
   validates_presence_of :user_id
-  validates_presence_of :content_id
-  validates_presence_of :content_type
   validates_presence_of :validity_starts_on
   validates_presence_of :validity_expires_on
 
   belongs_to :user
-  belongs_to :content, polymorphic: true
+  has_many   :library_contents
+  has_many   :movies, through: :library_contents, source: :content, source_type: 'Movie'
+  has_many   :seasons, through: :library_contents, source: :content, source_type: 'Season'
 end
--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Library, type: :model do
   it { should validate_presence_of(:user_id) }
-  it { should validate_presence_of(:content_id) }
-  it { should validate_presence_of(:content_type) }
   it { should validate_presence_of(:validity_starts_on) }
   it { should validate_presence_of(:validity_expires_on) }
   it { should belong_to(:user) }
-  it { should belong_to(:content) }
+  it { should have_many(:library_contents) }
+  it { should have_many(:movies).through(:library_contents).source(:content)}
+  it { should have_many(:seasons).through(:library_contents).source(:content)}
 end
