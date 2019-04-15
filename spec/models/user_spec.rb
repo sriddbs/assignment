0ba60643369d9a1e4ef8a92bfbaf436c01273069
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-04-15 15:41:16 +0530

    Update user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
+  it { should have_many(:purchases) }
+  it { should have_many(:libraries) }
+  it { should have_many(:library_contents).through(:libraries) }
+  it { should have_many(:active_contents).through(:libraries).source(:library_contents) }
 end
